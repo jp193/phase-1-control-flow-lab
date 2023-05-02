b754acc3cb474d033d83c859e4887e89b8da326a
Jamal Portericker <jportericker@yahoo.com>
2023-05-02 01:19:42 -0400

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  if (someValue >= 2500) {
+     return 'No can do.'
+   }
+  if (someValue < 400) {
+    return 'This one is on me!';
+  } else if (someValue > 400 && someValue < 2000) {
+    return 'That will be twenty bucks.'
+  } else {
+    return `I will gladly take your thirty bucks.`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city == 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+switch (tip) {
+  case 'generous':
+    return "Thank you so much.";
+    break;
+  case 'not as generous':
+    return "Thank you.";
+    break;
+  default:
+    return "Bye.";
+    break;
 }
+}
+console.log(switchOnCharmFromTip('not as generous'));
+// switch (groceryItem) {
+//     case 'tomato':
+//       console.log('Tomatoes are $0.49');
+//       break;
+//     case 'lime':
+//       console.log('Limes are $1.49');
+//       break;
+//     case 'papaya':
+//       console.log('Papayas are $1.29');
+//       break;
+//     default: 
+//       console.log('Invalid item');
+//       break;
+//   }
